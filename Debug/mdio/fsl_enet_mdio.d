8d271c9f8d552369fde863208e9be00a9f43e0fe
Maximiliano Mancilla <mmancilla.torres@iteso.mx>
2024-05-10 21:15:45 -0600

    CAN Canoe APP Baremetal. CANoe   8.2.80 (SP3)

---
+++ Debug/mdio/fsl_enet_mdio.d
@@ -0,0 +1,31 @@
+mdio/fsl_enet_mdio.o mdio/fsl_enet_mdio.d: ../mdio/fsl_enet_mdio.c \
+ ../mdio/fsl_enet_mdio.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_enet.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_common.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/fsl_device_registers.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/MK64F12.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/core_cm4.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/cmsis_version.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/cmsis_compiler.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/cmsis_gcc.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/system_MK64F12.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/MK64F12_features.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_common_arm.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_clock.h \
+ ../mdio/fsl_mdio.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_common.h
+../mdio/fsl_enet_mdio.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_enet.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_common.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/fsl_device_registers.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/MK64F12.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/core_cm4.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/cmsis_version.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/cmsis_compiler.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/cmsis_gcc.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/system_MK64F12.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/MK64F12_features.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_common_arm.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_clock.h:
+../mdio/fsl_mdio.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_common.h:
---
+++ Debug/phy/fsl_phyksz8081.d
@@ -0,0 +1,30 @@
+phy/fsl_phyksz8081.o phy/fsl_phyksz8081.d: ../phy/fsl_phyksz8081.c \
+ ../phy/fsl_phyksz8081.h ../phy/fsl_phy.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\mdio/fsl_mdio.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_common.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/fsl_device_registers.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/MK64F12.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/core_cm4.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/cmsis_version.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/cmsis_compiler.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/cmsis_gcc.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/system_MK64F12.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/MK64F12_features.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_common_arm.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_clock.h \
+ C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_common.h
+../phy/fsl_phyksz8081.h:
+../phy/fsl_phy.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\mdio/fsl_mdio.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_common.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/fsl_device_registers.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/MK64F12.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/core_cm4.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/cmsis_version.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/cmsis_compiler.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\CMSIS/cmsis_gcc.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/system_MK64F12.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\device/MK64F12_features.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_common_arm.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_clock.h:
+C:\Users\USER\Documents\MCUXpressoIDE_11.8.0_1165\workspace\frdmk64f_cmsis_enet_txrx_transfer\drivers/fsl_common.h:
